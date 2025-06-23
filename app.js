dc3b2edfc7d518a9cfc24a278054152c73ab5811
bigoluwagentle <olayiwolaolabode06@gmail.com>
2025-06-23 15:17:01 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,123 @@
+const button = document.querySelector("button");
+const ai = document.getElementById("ai");
+const x = document.getElementById("x");
+const ok = document.querySelectorAll(".ok");
+const iconHamburger = document.getElementById("icon-hamburger");
+const land = document.getElementById("land");
+const facebook = document.getElementById("facebook");
+const whatsapp = document.getElementById("Whatsapp");
+const web3 = document.getElementById("web3");
+const web = document.getElementById("web");
+const lan = document.getElementById("lan");
+const you = document.querySelectorAll(".you");
+const sun = document.getElementById("sun");
+const moon = document.getElementById("moon");
+const container = document.getElementById("container");
+const twi = document.getElementById("twi");
+const face = document.getElementById("face");
+const email = document.getElementById("email");
+const ecc = document.getElementById("ecc");
+const experience = document.getElementById("experience");
+const complete = document.getElementById("complete");
+const support = document.getElementById("Support");
+const faceboo = document.getElementById("faceboo");
+const twii = document.getElementById("twii");
+const face2 = document.getElementById("face2");
+function downloadPDF() {
+    const link = document.createElement('a');
+    link.download = 'Abdulmalik-CV.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+    }, {
+    threshold: 0.1
+});
+ok.forEach(section => {
+    observer.observe(section);
+});
+
+function addUrl(urlin){
+    let urlink = "";
+    urlink = urlin;
+    const link = document.createElement("a");
+    link.href = urlink;
+    link.target = "_blank";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+land.onclick = function(){
+    addUrl("https://manage-landing-page-vzqe.vercel.app");
+}
+facebook.onclick = function(){
+    addUrl("https://www.facebook.com/base.man.304047");
+}
+
+x.onclick = function(){
+    addUrl("https://x.com/broskiprop94216");
+}
+ai.onclick = function(){
+    addUrl("https://medicalai-wine.vercel.app");
+}
+whatsapp.onclick = function(){
+    addUrl("https://wa.me/9076956531");
+}
+web3.onclick = function(){
+    addUrl("https://xend-finance-seven.vercel.app");
+}
+web.onclick = function(){
+    addUrl("https://webapp-xi-five.vercel.app");
+}
+lan.onclick = function(){
+    addUrl("https://bagtaf-react.vercel.app");
+}
+email.onclick = function(){
+    // addUrl("mailto:abdrosheedabdmalikadgmail.com");
+    addUrl(window.location.href = "mailto:abdrosheedabdmalikadgmail.com");
+}
+ecc.onclick = function(){
+    addUrl("https://e-ccomerce-website-omega.vercel.app/");
+}
+twii.onclick = function(){
+    addUrl("https://x.com/broskiprop94216");
+}
+face2.onclick = function(){
+    addUrl("https://www.facebook.com/base.man.304047");
+}
+sun.onclick = function(){
+    sun.style.display = "none";
+    moon.style.display = "inline-block";
+    container.style.backgroundColor = "white";
+    document.querySelector("body").style.color = "#0f1117";
+    twi.src = "img/icon-twitter-dark.svg";
+    face.src = "img/icon-facebook-dark.svg";
+    experience.src = "img/user-3-svgrepo-com.svg";
+    complete.src = "img/complete.webp";
+    support.src = "img/icons8-support-51.png";
+    document.querySelectorAll("a").forEach(aa => {
+        aa.style.color = "#0f1117";
+    });
+}
+moon.onclick = function(){
+    sun.style.display = "inline-block";
+    moon.style.display = "none";
+    container.style.backgroundColor = "#0f1117";
+    document.querySelector("body").style.color = "white";
+    twi.src = "img/icon-twitter.svg";
+    face.src = "img/icon-facebook.svg";
+    experience.src = "img/icons8-popular-man-50.png";
+    complete.src = "img/icons8-check-mark.svg";
+    support.src = "img/icons8-support-50.png"
+    document.querySelectorAll("a").forEach(aa => {
+        aa.style.color = "white";
+    });
+}
